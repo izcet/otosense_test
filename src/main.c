6cc5aec0e2eee50029adc3f575d8fa4488b770be
Hackathon <hackathon@e1z4r9p3.42.us.org>
2018-01-24 11:50:50 -0800

    [urls.txt] [src/main.c]

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include "otosense.h"
 
+int     exit_and_close(int ret)
+{
+  Pa_terminate();
+  return (ret);
+}
+
 int			main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	return (0);
+  PaDeviceIndex   di;
+
+  Pa_Initialize();
+  di = Pa_GetDefaultInputDevice();
+  if (di == paNoDevice)
+    return (exit_and_close(1));
+
+
+
+  (void)argc;
+  (void)argv;
+  return (exit_and_close(0));
 }
